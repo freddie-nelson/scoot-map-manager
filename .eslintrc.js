07333d77e61394dcfcd426313ad7ba84de6df1ce
Freddie Nelson <freddie0208@hotmail.com>
2021-06-24 16:08:25 +0100

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/typescript/recommended",
+    "@vue/typescript",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
   ],
